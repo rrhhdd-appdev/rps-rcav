3aa3bc71e02c61ea4c5b88fddd509a3b31ceb1f7
rrhhdd <rharrisondavis@chicagobooth.edu>
2022-10-20 01:34:48 +0000

    rock, paper, ans scissors pages working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb" )
 
   # Add your actions below this line
   # ================================
 
   def play_rock
-   
     # redirect_to("https://www.wikipedia.org/")
+    @comp_move = ["rock", "paper", "scissors"].sample
 
-    # render({ :html => "<h1>Hello, World!</h1>".html_safe })
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
 
     render({ :template => "game_templates/user_rock.html.erb" })
   end 
@@ -29,6 +35,20 @@ class ApplicationController < ActionController::Base
     end
     
     render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    
+    render({ :template => "game_templates/user_scissors.html.erb" })
   end
 
 end
