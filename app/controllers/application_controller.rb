38393d4dd42d1f2f70c2723b0a2952afe7507916
rrhhdd <rharrisondavis@chicagobooth.edu>
2022-10-20 01:25:46 +0000

    Paper page progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,24 @@ class ApplicationController < ActionController::Base
     # render({ :html => "<h1>Hello, World!</h1>".html_safe })
 
     render({ :template => "game_templates/user_rock.html.erb" })
+  end 
+
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
   end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
 end
